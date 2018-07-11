1d4879c6a5740e11c250add61d2173daad86232a
Ahmed Eleaswy <elesawy325@yahoo.com>
2018-07-11 16:25:28 +0200

    Update e-typier.js

--- e-typier.js
+++ e-typier.js
@@ -2,7 +2,7 @@ function write(word, ele, fast) {
     "use strict";
 	var fullTime = (fast / word.length),
 		myEle = document.getElementById(ele),
-		x = 0;
+		x = -1;
 	setInterval(function () {
 		if (x < word.length - 1) {
 			x = x + 1;
